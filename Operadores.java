2bd8c694ae30a64482f56fdec9d936109f4f32c5
XavZol <alansolis707@gmail.com>
2026-02-16 09:07:53 -0600

    Operadores Aumento y Decremento.

---
+++ Operadores.java
@@ -0,0 +1,15 @@
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        int x = 5, y;
+        // decremento o aumento, colocandolo de subfijo o prefijo
+        y= x++;
+
+        System.out.println(x);
+        System.out.println(y);
+        
+
+    }
+
+}
