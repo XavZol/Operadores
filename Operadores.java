0e5af1070d667176b5d62e9324e3899835ddc2a5
XavZol <alansolis707@gmail.com>
2026-02-16 09:30:14 -0600

    Averiguar la cantidad de dinero

--- Operadores.java
+++ Operadores.java
@@ -1,22 +1,24 @@
+/*Luis tiene la mitad de lo que posee Guillermo.
+Juan tiene la mitad de lo que poseen Luis y Guillermo juntos
+Hacer un programa que calcule e imprima la cantidad de dinero que tienen entre los tres */
 
 import java.util.Scanner;
-
 
 public class Operadores {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int horasSemanales;
-        float salarioHora, salarioTotal;
+        float guillermo, luis, juan, total;
 
-        System.out.print("Digite las horas semanales trabajadas: ");
-        horasSemanales = entrada.nextInt();
-        System.out.print("Digite el salario que gana por hora: ");
-        salarioHora = entrada.nextFloat();
+        System.out.print("Digite la cantidad de dinero que tiene Guillermo: ");
+        guillermo = entrada.nextFloat();
 
-        salarioTotal = horasSemanales * salarioHora;
+        luis = guillermo/2;
+        juan = (guillermo+luis)/2;
 
-        System.out.println("\nEl salario semanal es: "+salarioTotal);
+        total = guillermo+luis+juan;
+
+        System.out.println("\nLa cantidad de dinero entre los 3 es: "+total);
     }
 
 }
