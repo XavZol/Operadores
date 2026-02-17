7ff9006e1a5a1c54c0b009ec7774d8034ce3f876
XavZol <alansolis707@gmail.com>
2026-02-17 10:58:45 -0600

    Determinar el cuadrado de una suma.

--- Operadores.java
+++ Operadores.java
@@ -1,35 +1,20 @@
-/* La calificación final de un estudiante de informática se calcula con base a las calificaciones 
-de cuatro aspectos de su rendimiento académico: participación, primer examen parcial, segundo examen parcial
-y examen final. Sabiendo que las calificaciones anteriores entran a la calificación final con ponderaciones 
-del 10%, 25%, 25% y 40%. Hacer un programa que calcule e imprima la calificación final por un estudiante. */
-
+/* Hacer un programa que calcule el cuadrado de  una suma  (a+b)^2 = a^2 + 2ab + b^2*/
 import java.util.Scanner;
 
 public class Operadores {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        float participacion, primerExamen, segundoExamen, examenFinal, notaFinal;
+        double a,b, resultado;
 
+        System.out.println("Digite el valor de a: ");
+        a = entrada.nextDouble();
+        System.out.println("Digite el valor de b: ");
+        b = entrada.nextDouble();
 
-        System.out.print("Digite la nota de participación: ");
-        participacion = entrada.nextFloat();
-        System.out.println("Digite la nota del primer examen parcial: ");
-        primerExamen = entrada.nextFloat();
-        System.out.println("Digite la nota del segundo examen parcial: ");
-        segundoExamen = entrada.nextFloat();
-        System.out.println("Digite la nota del examen final: ");
-        examenFinal = entrada.nextFloat();
+        resultado = Math.pow(a,2) + Math.pow(b,2) + (2*a*b);
 
-        participacion *= 0.10f;
-        primerExamen *= 0.25f;
-        segundoExamen *= 0.25f;
-        examenFinal *= 0.40f;
-
-        notaFinal = participacion + primerExamen + segundoExamen + examenFinal;
-
-        System.out.println("La nota final del estudiante es: "+notaFinal);
-        
+        System.out.println("El resultado es: "+resultado);
     }
 
 }
